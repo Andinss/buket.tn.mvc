ec0a62443632959d152ba43b4d57b92b87c3c16f
Andini <andini@siklus.id>
2025-10-21 09:53:49 +0700

    build apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            
+            // Disable minify dan shrink
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
